e27265008b0db69cdac25b6492c77be8e500f393
Tia Rose <shortyshort789@aol.com>
2020-05-14 21:59:48 -0700

    cors

--- index.js
+++ index.js
@@ -29,13 +29,20 @@ app.use(cors(
 	})
 );
 //cors
-app.options('/user', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Allow-Headers', '*');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	res.end();
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Credentials', true);
+	next();
 });
+// app.options('/user', function (req, res) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', '*');
+// 	res.setHeader('Access-Control-Allow-Headers', '*');
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	res.end();
+// });
 
 
 // use routes
