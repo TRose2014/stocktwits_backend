7adca331e0f822b1a2a0cfc19ccd9adc95a07d6d
Tia Rose <shortyshort789@aol.com>
2020-05-14 21:54:34 -0700

    working with cors

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.options('/user', function (req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', '*');
 	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.end();
 });
 
