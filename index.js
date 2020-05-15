3614f1362322d39ca46841db9ec4ad8838df4747
Tia Rose <shortyshort789@aol.com>
2020-05-15 16:11:50 -0700

    cors

--- index.js
+++ index.js
@@ -21,15 +21,35 @@ app.use(session(
 	})
 );
 
+var allowedOrigins = ['http://localhost:3000',
+	'https://infallible-booth-e191ee.netlify.app',
+	'https://infallible-booth-e191ee.netlify.app/',
+];
+app.use(cors({
+	credentials: true,
+	methods: 'GET',
+	origin: function(origin, callback){
+		// allow requests with no origin 
+		// (like mobile apps or curl requests)
+		if(!origin) return callback(null, true);
+		if(allowedOrigins.indexOf(origin) === -1){
+			var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	}
+}));
+
 // configure CORS
-app.use(cors(
-	{
-		origin: 'https://infallible-booth-e191ee.netlify.app',
-		credentials: true,
-		methods: 'GET',
+// app.use(cors(
+// 	{
+// 		origin: 'https://infallible-booth-e191ee.netlify.app',
+// 		credentials: true,
+// 		methods: 'GET',
     
-	})
-);
+// 	})
+// );
 //cors
 // app.use(function(req, res, next) {
 // 	res.header('Access-Control-Allow-Origin', '*');
