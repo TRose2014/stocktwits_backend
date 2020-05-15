8ca9252cb50ff358d88993e3fa6df2e67187d732
Tia Rose <shortyshort789@aol.com>
2020-05-14 23:13:22 -0700

    cors

--- routes/oauth-callback.js
+++ routes/oauth-callback.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 
 router.get('/', (req, res) => {
 	console.log('Inside oauth-callback');
-	console.log('req', req.query);
+	console.log('req-query', req.query);
+	console.log('res', res);
 	request(
 		{
 			method: 'POST',
@@ -17,11 +18,10 @@ router.get('/', (req, res) => {
 			console.log('body2', body);
 			// save token to session
 			req.session.token = JSON.parse(body).access_token;
-			req.session.data = body.results;
-			res.send(req.session.data);
+			res.send({res:body.results});
 
 			// redirect to the React app
-			// res.redirect('https://infallible-booth-e191ee.netlify.app');
+			res.redirect('https://infallible-booth-e191ee.netlify.app');
 		}
 	);
 });
