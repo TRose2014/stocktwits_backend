ef989c5ccf5247f424ca714d0b4fb9afe4945569
Tia Rose <shortyshort789@aol.com>
2020-05-15 21:35:31 -0700

    working on CORS

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
 	// });
 
 	console.log('req2', req.session);
+	console.log('req3', req.session.token);
 
 	if(req.session.token) {
 		request(
@@ -26,7 +27,7 @@ router.get('/', (req, res) => {
 				req.session.data = body.results;
 
 				// redirect to the React app
-				res.redirect('https://infallible-booth-e191ee.netlify.app/');
+				res.redirect('http://localhost:3000/');
 			}
 
 		);
