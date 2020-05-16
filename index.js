d1d9f2d7d94add633720108db4ff8833ab4e40c8
Tia Rose <shortyshort789@aol.com>
2020-05-15 18:50:06 -0700

    working on CORS

--- index.js
+++ index.js
@@ -43,12 +43,7 @@ app.use(session(
 // }));
 
 // configure CORS
-app.use(cors(
-	{
-		origin: 'https://infallible-booth-e191ee.netlify.app'
-    
-	})
-);
+app.use(cors());
 //cors
 // app.use(function(req, res, next) {
 // 	res.header('Access-Control-Allow-Origin', '*');
