f22361b5d0117ea9346a1cfc79be7c013ac23a19
Tia Rose <shortyshort789@aol.com>
2020-06-16 22:52:13 -0700

    cors

--- index.js
+++ index.js
@@ -21,7 +21,27 @@ app.use(session(
 	})
 );
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000',
+	'https://infallible-booth-e191ee.netlify.app',
+	'https://infallible-booth-e191ee.netlify.app/',
+	'http://localhost:3000/',
+	'https://stockwits-backend.herokuapp.com/user',
+	'https://stockwits-backend.herokuapp.com/',
+];
+app.use(cors({
+	credentials: true,
+	methods: 'GET',
+	origin: function(origin, callback){
+	// allow requests with no origin 
+		if(!origin) return callback(null, true);
+		if(allowedOrigins.indexOf(origin) === -1){
+			const msg = 'The CORS policy for this site does not ' +
+                 'allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	} 
+}));
 
 // use routes
 app.use('/user', require('./routes/user'));
